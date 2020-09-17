1bef9dbe3acf0c023f3837dbea6aa79109abe99d
Radosav Leovac <rleovac@gmail.com>
2020-09-17 16:09:15 +0200

    s

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark py-4 bg-custom shadow mb-5">
-    <a class="navbar-brand" href="#">Word Counter</a>
+    <a class="navbar-brand" href="#">Word  Counter & Calculator</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
