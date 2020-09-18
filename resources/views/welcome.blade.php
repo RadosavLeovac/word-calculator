9610316f555fc936a31b166453132c28009eb2b0
Radosav Leovac <rleovac@gmail.com>
2020-09-18 21:20:28 +0200

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,32 @@
 <body>
 @include('partials.header')
 <div class="container" id="app">
-    <example-component></example-component>
+    <example-component>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 mb-3">
+                    <h1 class=""> Word Counter and Calculator </h1>
+                    <p class="h2 indigo font-weight-bold">Easily calculate the number of the words and your rate.</p>
+                    <hr class="shadow">
+                </div>
+                <div class="col-md-12">
+                    <div class="alert alert-primary shadow-sm mb-3 h3 font" role="alert">
+                        Words: <span class="font-weight-bold">@{{ wcount }} |</span> Characters: <span class="font-weight-bold">@{{ ccount }} |</span> Sentences: <span class="font-weight-bold">@{{ scount}}</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="rate">Enter your rate per 1000 words <small class="text-secondary">EUR / USD</small></label>
+                        <input class="form-control shadow-sm mb-3" id="rate" type="number" @input="charCount()" v-model="rate">
+                        <p class="h4">Total amount: @{{ price }} <small class="text-muted">EUR / USD</small></p>
+                    </div>
+                    <div class="form-group">
+                        <label for="input"></label>
+                        <textarea v-model="userinput" @keyup='charCount()' class="form-control shadow" name="" id="input"
+                                  cols="30" rows="15"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </example-component>
 </div>
 @include('partials.footer')
 </body>
